817e37c4325da95c76bded428d9fb68a523814d1
edmondyuen917 <edmond917@yahoo.com.hk>
2019-10-25 15:31:18 +0800

    feature 1

--- app/src/main/java/com/kct/bluetooth_demo/MainActivity.java
+++ app/src/main/java/com/kct/bluetooth_demo/MainActivity.java
@@ -5,7 +5,6 @@ import android.Manifest;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -88,6 +87,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /*
+    feature 1
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
